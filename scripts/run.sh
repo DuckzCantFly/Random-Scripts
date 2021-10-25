a0374ebc8c7aa7d5e7c459e861a2a5f790c06557
Duckz@protonmail.ch <Duckz@protonmail.ch>
2021-10-25 14:18:54 -0600

    cleanup

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+# a little script to try to fix rpgmaker games
 
 if [ -z "$1" ] ; then
 	GAMELOC="$(echo "$0" | rev | cut -d '/' -f 2- | rev)"\
@@ -10,3 +11,7 @@ fi
 	&& chmod +x "$GAMELOC/nw"
 
 "$GAMELOC/nw"
+
+# if that doesnt work try :
+# mkdir fonts; echo '<fontconfig><dir>/usr/share/fonts</dir><cachedir>fonts</cachedir></fontconfig>' >fonts/fonts.conf; FONTCONFIG_PATH=fonts ./nw # %command%
+# from https://forums.rpgmakerweb.com/index.php?threads/playtest-not-working-on-ubuntu-18-04.94710/
