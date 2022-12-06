ee523cd10384f77d330ee1765d3f20d0e70fb0bf
DuckzCantFly <30376517+DuckzCantFly@users.noreply.github.com>
2022-12-05 23:05:01 -0700

    more cheesing with chatgpt

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,16 +13,24 @@ set_game_directory() {
 # Find version what of nw to use
 find_perfered_version() {
 	# Check for the latest tag of NW.js
-	# local latest_tag=$(curl -s https://api.github.com/repos/nwjs/nw.js/tags | jq -r '.[0].name')
-	local latest_tag=$(curl -s https://api.github.com/repos/nwjs/nw.js/tags | grep "name" | cut -d '"' -f 4 | head -n 1)
-	
+	local latest_tag=$(curl -s https://api.github.com/repos/nwjs/nw.js/tags | jq -r '.[0].name')
+
 	# Extract the version number from the tag	
 	local latest_version=$(echo "$latest_tag" | cut -d 'v' -f 2)
-	
+
 	# Check if the game has a "package.json" file
 	if [ -f "$1/package.json" ]; then
 		# Extract the preferred version of NW.js from the "package.json" file
-		echo $(cat "$1/package.json" | grep "nwjs-sdk-v" | cut -d '"' -f 4 | cut -d '-' -f 3)
+		local preferred_version=$(cat "$1/package.json" | grep "nwjs-sdk-v" | cut -d '"' -f 4 | cut -d '-' -f 3)
+	
+		# Check if the preferred version is valid
+		if [ -z "$preferred_version" ] || [ "$preferred_version" -lt "0" ]; then
+			# Use the latest version if the preferred version is not found or is not valid
+			echo $latest_version
+		else
+			# Use the preferred version if it is found and valid
+			echo $preferred_version
+		fi
 	else
 		# Use the latest version of NW.js if no "package.json" file is found
 		echo $latest_version
@@ -61,15 +69,24 @@ generate_font_config() {
 		else
 			# Use a hard-coded fallback directory if /usr/share/fonts is not available
 			echo '<fontconfig><dir>/usr/share/fonts</dir><cachedir>fonts</cachedir><match target="font"><edit name="antialias" mode="assign"><bool>true</bool></edit><edit name="hinting" mode="assign"><bool>true</bool></edit><edit name="hintstyle" mode="assign"><const>hintslight</const></edit><edit name="rgba" mode="assign"><const>rgb</const></edit><edit name="lcdfilter" mode="assign"><const>lcddefault</const></edit></match></fontconfig>' > "$1/fonts
-/fonts.conf"
+
+	/fonts.conf"
 		fi
 	else
-	# Display an error message if the fontconfig library is not available
-	echo "Error: the fontconfig library is not installed or not available on the system"
-	echo "Please make sure that the fontconfig library is installed and available before running this script"
-	exit 1
-fi
+		# Check if the /usr/share/fonts directory exists and is readable
+		if [ -d "/usr/share/fonts" ] && [ -r "/usr/share/fonts" ]; then
+			# Use a hard-coded fallback directory if the fontconfig library is not available
+			echo '<fontconfig><dir>/usr/share/fonts</dir><cachedir>fonts</cachedir><match target="font"><edit name="antialias" mode="assign"><bool>true</bool></edit><edit name="hinting" mode="assign"><bool>true</bool></edit><edit name="hintstyle" mode="assign"><const>hintslight</const></edit><edit name="rgba" mode="assign"><const>rgb</const></edit><edit name="lcdfilter" mode="assign"><const>lcddefault</const></edit></match></fontconfig>' > "$1/fonts
+			/fonts.conf"
+		else
+			# Display an error message if the /usr/share/fonts directory is not available
+			echo "Error: the /usr/share/fonts directory does not exist or is not readable"
+			echo "Please make sure that the /usr/share/fonts directory exists and is readable before running this script"
+			exit 1
+		fi
+	fi
 }
+
 
 # Run the game with the specified font configuration
 run_game() {
